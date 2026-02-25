f2f38966be9deda27a4dda2834170b3fe81f9705
SAMIYAS SOLOMON <138712866+sami2995@users.noreply.github.com>
2026-02-25 16:07:33 +0300

    Register models in Django admin and update WSGI configuration

--- rental/admin.py
+++ rental/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Car, Rental
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Car)
+admin.site.register(Rental)
